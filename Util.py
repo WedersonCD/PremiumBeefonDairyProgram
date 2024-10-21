b11ce0973a8b0fa49284b63cd74295a3e81131ca
wedersonCD <wedersoncabral@gmail.com>
2024-10-21 18:37:56 -0300

    create controller file

--- Util.py
+++ Util.py
@@ -1,6 +1,14 @@
 import csv
 
-def generate_csv_with_out_heads(content,filePath):
-        with open(filePath, mode='w', newline='') as file:
+def generate_csv_with_out_heads(content,file_path):
+        with open(file_path, mode='w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerows(content)
+            writer.writerow(content)
+
+def get_file_content(file_path):
+       with open(file_path, mode='r') as file:
+        reader = csv.reader(file)
+        
+        # Iterate through the rows and append to the content list
+        for row in reader:
+            return row
